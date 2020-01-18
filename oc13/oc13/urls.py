44b5a0bf5d943a5ac2e461dd332a6542d1bada03
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-01-18 16:03:13 +0100

    Add PasswordResetView feature

--- oc13/oc13/urls.py
+++ oc13/oc13/urls.py
@@ -20,10 +20,19 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('core.urls')),
     path('', include('registration.urls')),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-         name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-         name='password_change_done'),
-
     path('admin/', admin.site.urls),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),
+         name='password_reset'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+
 ]
