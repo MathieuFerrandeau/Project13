0e93411210c5fb722bd76bf94078aa331741280b
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-02-04 18:14:50 +0100

    Templates Update

--- oc13/oc13/settings/production.py
+++ oc13/oc13/settings/production.py
@@ -14,7 +14,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
