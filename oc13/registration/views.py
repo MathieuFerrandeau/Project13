4d560689bc3b5a134307891ba39bec0be0464efc
Mathieu digitalocean serveur <crypto.mathieuf@gmail.com>
2020-02-04 18:06:59 +0000

    mise a niveau

--- oc13/registration/views.py
+++ oc13/registration/views.py
@@ -79,7 +79,7 @@ def emailView(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['assitance.oc13@gmail.com'])
+                send_mail(subject, message, from_email, ['assistance.oc13@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('registration:success')
